956d38c88734866798365d39e2d4a6cfa3813a49
AsBaZa <asier.badiola@outlook.es>
2022-11-26 11:25:16 +0100

    Crear el componente `Book`

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { useState } from "react";
+import Book from "./Book";
 
 function App() {
   const [showSearchPage, setShowSearchpage] = useState(false);
@@ -109,29 +110,7 @@ function App() {
                 <div className="bookshelf-books">
                   <ol className="books-grid">
                     {books.currentlyReading.map((book) => {
-                      return (
-                        <li>
-                          <div className="book">
-                            <div className="book-top">
-                              <div
-                                className="book-cover"
-                                style={book.image}
-                              ></div>
-                              <div className="book-shelf-changer">
-                                <select>
-                                  <option value="none" disabled> Move to...</option>
-                                  <option value="currentlyReading">Currently Reading</option>
-                                  <option value="wantToRead">Want to Read</option>
-                                  <option value="read">Read</option>
-                                  <option value="none">None</option>
-                                </select>
-                              </div>
-                            </div>
-                            <div className="book-title">{book.title}</div>
-                            <div className="book-authors">{book.author}</div>
-                          </div>
-                        </li>
-                      );
+                      return (<Book book={book}/>)
                     })}
                   </ol>
                 </div>
@@ -141,29 +120,7 @@ function App() {
                 <div className="bookshelf-books">
                   <ol className="books-grid">
                     {books.wantToRead.map((book) => {
-                      return (
-                        <li>
-                          <div className="book">
-                            <div className="book-top">
-                              <div
-                                className="book-cover"
-                                style={book.image}
-                              ></div>
-                              <div className="book-shelf-changer">
-                                <select>
-                                  <option value="none" disabled> Move to...</option>
-                                  <option value="currentlyReading">Currently Reading</option>
-                                  <option value="wantToRead">Want to Read</option>
-                                  <option value="read">Read</option>
-                                  <option value="none">None</option>
-                                </select>
-                              </div>
-                            </div>
-                            <div className="book-title">{book.title}</div>
-                            <div className="book-authors">{book.author}</div>
-                          </div>
-                        </li>
-                      );
+                      return (<Book book={book}/>)
                     })}
                   </ol>
                 </div>
@@ -173,29 +130,7 @@ function App() {
                 <div className="bookshelf-books">
                   <ol className="books-grid">
                     {books.read.map((book) => {
-                      return (
-                        <li>
-                          <div className="book">
-                            <div className="book-top">
-                              <div
-                                className="book-cover"
-                                style={book.image}
-                              ></div>
-                              <div className="book-shelf-changer">
-                                <select>
-                                  <option value="none" disabled> Move to...</option>
-                                  <option value="currentlyReading">Currently Reading</option>
-                                  <option value="wantToRead">Want to Read</option>
-                                  <option value="read">Read</option>
-                                  <option value="none">None</option>
-                                </select>
-                              </div>
-                            </div>
-                            <div className="book-title">{book.title}</div>
-                            <div className="book-authors">{book.author}</div>
-                          </div>
-                        </li>
-                      );
+                      return (<Book book={book}/>)
                     })}
                   </ol>
                 </div>
